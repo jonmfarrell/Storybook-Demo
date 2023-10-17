29353b5204f5fd11b63a2ffd33d9def34b8d41bf
Jon Farrell <7273307+jonmfarrell@users.noreply.github.com>
2023-10-17 14:02:47 -0400

    Update Accordion.js

--- stories/Accordion.js
+++ stories/Accordion.js
@@ -4,7 +4,7 @@ export const createAccordion = () => {
  
     const accordion = `
 <h3>
-    <button aria-expanded="true" aria-controls="accordion-panel" id="accordion-button">Accordion Button</button>
+    <button aria-expanded="true" aria-controls="accordion-panel" id="accordion-button">Button</button>
 </h3>
 <div id="accordion-panel" role="region" aria-labelledby="accordion-button" class="accordion-panel">
     <p>Litora conubia at fringilla odio semper elementum condimentum ullamcorper ad vitae condimentum volutpat iaculis tellus parturient dui sagittis odio adipiscing taciti. Ligula condimentum a fames dui tempor pharetra a consectetur a suspendisse lacus tristique a a duis mi dis a curae.</p>
